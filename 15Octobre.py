72ee4fa79c8b5f305cbe82d72d5161d6da32e86d
DAVID lucas <lucas.david44000@gmail.com>
2020-11-04 08:10:05 +0100

    15 Octobre, Dichotomie MPSI Dichotomie fonction cos(x)=x

--- 15Octobre.py
+++ 15Octobre.py
@@ -1,20 +1,18 @@
 import numpy as np
 
+
 def f(x):
-    return x - np.cos(x)
+    return np.log(x)-np.cos(x)
+
+def dicho(f, a, b, ecart):
+    aa,bb= a,b
+    while np.abs(bb - aa) > ecart:
+        c = (aa + bb) / 2
+        if f(c) < 0:
+            aa = c
+        elif f(c) > 0:
+            bb = c
+    return [aa, bb, a, b]
 
 
-a=0
-b= np.pi/2
-
-while np.abs(b - a) > 1/1000:
-    c = (a + b) / 2
-    if f(c) < 0:
-        a = c
-    elif f(c) > 0:
-        b = c
-
-
-print(a, b)
-
-
+print(dicho(f,0.0001,np.pi/2-0.0001, 0.0001))
